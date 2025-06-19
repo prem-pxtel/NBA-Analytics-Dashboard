9ad59f7fb7642b6316d658d810d02839a9437ad0
Anna Takegawa <takegawaanna@gmail.com>
2025-06-19 19:50:09 -0400

    made main.py run on localhost:5000

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+
+load_dotenv()
+app = Flask(__name__)
 
 
-def run_sql_file(path: str):
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER")
+        # password=os.getenv("DB_PWD")
+    )
+
+
+def run_sql_file(cursor, db, path: str):
     with open(path, 'r') as sql_file:
         sql = sql_file.read()
         cursor.execute(sql)
@@ -17,35 +31,23 @@ def load_query(path: str):
     return [q.strip() for q in sql.split(';') if q.strip()]
 
 
-def test_sample(player_id: int):
-    # insert sample data
-    run_sql_file("sql/sample_data.sql")
-    print("Sample data loaded")
-
-    # load queries and set param for each
+def test_sample(cursor, player_id: int, season_id: int, game_id: int, stat: str):
+    # load queries
     test_queries = load_query("sql/test-sample.sql")
-    params_list = [
-        {"player_id": 1, "season_id": 2003},   # R6
-        {"player_id": 1, "game_id": 1},     # R7
-        {"player_id": 1, "stat": "points"},  # R8
-        {}                                  # R9
-    ]
 
     results = []
 
     for i, query in enumerate(test_queries):
-        param = params_list[i]
         if i == 0:
-            query = query.format(player_id=param['player_id'], season_id=param['season_id'])
+            query = query.format(player_id=player_id, season_id=season_id)
         elif i == 1:
-            query = query.format(player_id=param['player_id'], game_id=param['game_id'])
-
+            query = query.format(player_id=player_id, game_id=game_id)
         elif i == 2:
-            query = query.format(player_id=param['player_id'], stat=param['stat'])
+            query = query.format(player_id=player_id, stat=stat)
         elif i == 3:
             pass
 
-        cursor.execute(query, param)
+        cursor.execute(query)
         results.append(cursor.fetchall())
 
     # print result
@@ -54,24 +56,42 @@ def test_sample(player_id: int):
 
     return results
 
+# API endpoint
 
-if __name__ == "__main__":
-    load_dotenv()
+
+@app.route("/api/player_stats", methods=["GET"])
+def get_player_stats():
+    # get query parameters
+    try:
+        player_id = int(request.args.get("player_id"))
+        season_id = int(request.args.get("season_id"))
+        game_id = int(request.args.get("game_id"))
+        stat = request.args.get("stat")
+    except:
+        return jsonify({"error": "Missing or invalid parameters"}), 400
 
     # connect to database
-    db = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER")
-        # password=os.getenv("DB_PWD")
-    )
+    db = get_db_connection()
     cursor = db.cursor()
     print("Connected to database")
 
-    # create tables
-    run_sql_file("sql/create_tables.sql")
+    # create & load tables
+    run_sql_file(cursor, db, "sql/create_tables.sql")
     print("Created tables")
+    run_sql_file(cursor, db, "sql/sample_data.sql")
+    print("Loaded tables")
 
     # run test
-    test_sample(player_id=1)
+    results = test_sample(cursor, player_id, season_id, game_id, stat)
+
+    db.close()
+    return jsonify({
+        "r6_player_season_stats": results[0],
+        "r7_player_game_stats": results[1],
+        "r8_best_game_by_stat": results[2],
+        "r9_all_time_top_scorers": results[3]
+    })
+
+
+if __name__ == "__main__":
+    app.run(host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"))
